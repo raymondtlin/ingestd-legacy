df978c77c8ae08934621ba2112dc4b88a72ac75f
raymondtlin <ray.ktl1n+git@gmail.com>
2019-10-01 01:58:19 -0700

    containers

---
+++ src/QuerySchema.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+import boto3
+
+from conf import rds
+
+# set clients
+s3 = boto3.client('s3')
+rds_client = boto3.client('rds-data')
+
+
+@dataclass
+class TableSchema:
+    # namespace: str
+    # name: str
+    # type: str
+    # fields: List[Field]
+    # Field: NamedTuple('Field', 'field_name, field_type')
+    query: str
+
+    def fetch(self):
+        sql = self.query
+
+        response = rds_client.execute_statement(sql, **rds.params)
+        for record in response['records']:
+            print(record)

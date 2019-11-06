de402f0ed170de11d0c9f5cf11ffcfcf1032a8da
Raymond T. Lin <ray.ktl1n+git@gmail.com>
2019-11-05 18:36:17 -0800

    Operators

--- ingestd/Operators.py
+++ ingestd/Operators.py
@@ -1,25 +1,26 @@
-from kafka.SchemaRegistryAbstraction import Schema, JSONSchema
+from __future__ import absolute_import
+from ingestd.kafka.SchemaRegistryAbstraction import Schema, JSONSchema
 from pathlib import Path
 from confluent_kafka import Message
+from ingestd.kafka.finwire.utils import find_type
 
 
 class FlatFileConnector(object):
-    def __init__(self, config, **kwargs):
+    def __init__(self, **kwargs):
         """
         :param str file_path:
-        :param str file_format: "fwf, csv, tsv, bar, xml"
+        :param str file_format: "fwf, csv, tsv, bar, xml, txt"
         """
-        self.CONFIG = config
-        self.file_path = kwargs['file_path']
-        self.file_format = kwargs['file_format']
-        self.file_type = self.file_path.split('\\.')[-1]
-        self.delimiter = kwargs['delimiter']
+        self.file_path = kwargs['file_path'] or None
+        self.file_format = kwargs['file_format'] or None
+        self.file_type = self.file_path.split('\\.')[-1] or None
+        self.delimiter = kwargs['delimiter'] or None
 
-    def route():
-        if self.file_format in ["csv", "tsv", "bar"]:
-            operator = DelimiterOperator(self.delimiter)
+    def route(self):
+        if self.file_format in ["csv", "tsv", "bar", "txt"]:
+            operator = DelimiterOperator(self.file_path, self.delimiter)
         elif self.file_format == 'xml':
-            operator = XMLOperator()
+            operator = XMLOperator(self)
         elif self.file_format == 'fwf':
             operator = FixedWidthOperator()
         else:
@@ -45,20 +46,20 @@ class Record(object):
         self.key_schema = key_schema
         self.value_schema = value_schema
 
-    # Needs revision
-    for schema in [self.key_schema, self.value_schema]:
-        if schema:
-            try:
-                sch = JSONSchema(schema).to_avro()
-                try:
-                    sch.register(schema_registry_url=self.schema_registry_url)
+    # # Needs revision
+    # for schema in [self.key_schema, self.value_schema]:
+    #     if schema:
+    #         try:
+    #             sch = JSONSchema(schema).to_avro()
+    #             try:
+    #                 sch.register(schema_registry_url=self.schema_registry_url)
 
-                except BaseException as e:
-                    print('Could not register schema in the registry', e)
-                finally:
-                    self.valid_schemas.append(sch)
-            except Exception as e:
-                print('Could not convert schema to avro')
+    #             except BaseException as e:
+    #                 print('Could not register schema in the registry', e)
+    #             finally:
+    #                 self.valid_schemas.append(sch)
+    #         except Exception as e:
+    #             print('Could not convert schema to avro')
 
 
 class StringOperator:
@@ -69,28 +70,28 @@ class StringOperator:
         yield(record)
 
 
-class DelimiterOperator(FlatFileConnector):
-    def __init__(self):
-        pass
-
-    def apply():
+class DelimiterOperator(object):
+    def __init__(self, file_path=None, delimiter=None):
+        self.file_path = file_path
+        self.delimiter = delimiter
+    
+    def apply(self):
         while True:
             with open(self.file_path, 'r') as stream:
                 for line in stream.readlines():
                     record = Record(iterable=line.split(self.delimiter))
-                    if key_schema:
-                        key = record.apply(key_schema)
-                    if value_schema:
-                        value = record.apply(value_schema)
-                    yield record, key, value
+                    # if key_schema:
+                    #     key = record.apply(key_schema)
+                    # if value_schema:
+                    #     value = record.apply(value_schema)
+                    yield record
 
 
 class FixedWidthOperator(FlatFileConnector):
+    def __init__(self):
+        super(FlatFileConnector, self).__init__(**kwargs)
 
-    def __init__(self, fieldWidths):
-        self._fieldwidths = fieldWidths
-
-    def create_parser():
+    def create_parser(self, fieldWidths):
         # https://stackoverflow.com/a/4915359
         """
         Constructs a tuple of fieldwidths to parse fixed-width records
@@ -98,13 +99,13 @@ class FixedWidthOperator(FlatFileConnector):
         :return: parsed line as List[string]
         """
         cuts = tuple(cut for cut in accumulate(
-            abs(fw) for fw in self._fieldwidths))
-        pads = tuple(fw < 0 for fw in self._fieldwidths)
+            abs(fw) for fw in fieldWidths))
+        pads = tuple(fw < 0 for fw in fieldWidths)
         flds = tuple(zip_longest(pads, (0,) + cuts, cuts))[:-1]
 
         return lambda line: tuple(line[i: j] for pad, i, j in flds if not pad)
 
-    def apply(field_widths):
+    def apply(self):
         """
         Opens a fs handle to specified path.  Reads each line in the flatfile.
         Determines type of record.
